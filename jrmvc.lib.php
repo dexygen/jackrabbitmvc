d2c9e46dc1590b774ec28e9e65532ead6907ef8d
George Jempty <consult@dexygen.com>
2019-01-03 03:13:39 +0000

    Brought back from fossil repo and added convenience method, first significant change in 12 years

---
+++ jrmvc.lib.php
@@ -0,0 +1,118 @@
+<?php
+  /*
+   * jrMvc (JackRabbitMvc -- formerly barebonesmvc-php) 
+   * is a one-file MVC micro-framework for PHP5.
+   * 
+   * Copyright (c) 2007, George M. Jempty
+   *
+   * "A designer knows he has achieved perfection not when there is nothing left
+   * to add, but when there is nothing left to take away."
+   * (Antoine de Saint-Exupery)
+   *
+   * LICENSE: https://opensource.org/licenses/MIT
+   *
+   * USAGE:
+   *
+   require('jrmvc.lib.php');                                 // 1) require
+    
+   class DemoController extends AbstractJrMvcController {    // 2) extend
+      function applyInputToModel() {                         // 3) implement
+         $mto = new JrMvcMTO('jrmvc.tpl.php');               // 4) instantiate
+         // jrmvc.tpl.php content: <pre>$model: <?php print_r($model); ?></pre>
+   
+         $mto->setModelValue('Su', 'Sunday');                // 5) assignments
+         $mto->setModelValue('Mo', 'Monday');
+         $mto->setModelValue('Tu', 'Tuesday');
+         $mto->setModelValue('We', 'Wednesday');
+         $mto->setModelValues(['Th'=>'Thursday', 'Fr'=>'Friday', 'Sa'=>'Saturday']);
+   
+         return $mto;                                        // 6) return
+       }
+   }
+    
+   DemoController::sendResponse(new DemoController());       // 7) send
+   *
+   * OUTPUT:
+   *
+     $model: Array
+      (
+      [Su] => Sunday
+      [Mo] => Monday
+      [Tu] => Tuesday
+      [We] => Wednesday
+      [Th] => Thursday
+      [Fr] => Friday
+      [Sa] => Saturday
+      )
+   */ 
+
+  interface IJrMvcController {
+    function setMto(IModelXfer $mto);
+    static function sendResponse(IJrMvcController $controller);
+    function applyInputToModel();
+  }
+  
+  abstract class AbstractJrMvcController implements IJrMvcController {
+    protected $mto;
+    
+    function setMto(IModelXfer $mto) {
+      $this->mto = $mto;
+    }
+    
+    static function sendResponse(IJrMvcController $controller) {
+      $controller->setMto($controller->applyInputToModel());
+      $controller->mto->applyModelToView();
+    }
+  }
+  
+  interface IModelXfer {
+    function setView($view);
+    function setModel($model);
+    function setModelValue($key, $value);
+    function applyModelToView();
+  }
+
+  abstract class AbstractMTO implements IModelXfer {
+    protected $view;
+    protected $model;
+    
+    function setView($view) {
+      $this->view = $view;    
+    }
+    
+    function setModel($model) {
+      $this->model = $model;
+    }
+    
+    function setModelValue($key, $value) {
+      $this->model[$key] = $value;
+    }
+    
+    function setModelValues($arr) {
+      foreach ($arr as $key => $value) {
+        $this->model[$key] = $value;
+      }
+    }
+    
+    protected function unsetNonSessionGlobals() {
+      $session = $GLOBALS['_SESSION'];
+      unset($GLOBALS);
+      $GLOBALS['_SESSION'] = $session;      
+    }
+  }
+  
+  class JrMvcMto extends AbstractMTO {    
+    function __construct($view) {
+      $this->setView($view);
+    }
+    
+    function applyModelToView() {
+      # Ensures view does not have access to get/post variables,
+      # thus encouraging all access to them to occur within controller
+      $this->unsetNonSessionGlobals();
+      
+      $model = $this->model;
+      include($this->view);
+    }    
+  }
+?>
